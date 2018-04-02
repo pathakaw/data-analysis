2f9c8f22943f8db5eb7ebc1d1bd50deb6b552f3e
Awadhesh Pathak <38004343+pathakaw@users.noreply.github.com>
2018-04-02 17:30:02 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='data-analysis',
+      version='1.0',
+      description='Python Twitter Data Analysis',
+      author='Awadhesh Pathak',
+      license='MIT',
+      url='https://www.github.org/pathakaw/data-analysis',
+      packages=find_packages(),
+      install_requires=[],
+      entry_points={},
+      extras_require={'dev': ['flake8,]},
+     )
